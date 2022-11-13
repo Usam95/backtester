bce1eb98d81d5a68e81333ccc4357a7af41a63cc
Usam95 <usam.sersultanov@gmail.com>
2022-11-13 15:06:34 +0100

    added modul to store and plot the back/forward testing results

---
+++ binance/utilities/DataPlot.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+base_str_data = ['ticker', 'indicator', 'dataset',  'num_trades', 'strat_multiple',
+                 'bh_multiple', 'outperf', 'cagr', 'ann_mean', 'ann_std', 'sharpe', 'sortino',
+                 'max_dd', 'calmar', 'max_dd_dur', 'kelly_crit', 'start_d',
+                 'end_d', 'num_samples']
+
+
+class DataPlot:
+
+    def __init__(self, dataset, indicator, symbol):
+        self.dataset = dataset
+        self.indicator = indicator
+        self.symbol = symbol
+        self.init_df()
+
+    def init_df(self):
+        self.df = pd.DataFrame(columns=base_str_data)
+        #self.df.set_index('performance', inplace=True)
+
+    def store_testcase_data(self, perf_obj, params):
+        testcase_data = []
+        testcase_data.append(self.symbol)
+        testcase_data.append(self.indicator)
+        testcase_data.append(self.dataset)
+        testcase_data.append(perf_obj.num_of_trades)
+        testcase_data.append(perf_obj.strategy_multiple)
+        testcase_data.append(perf_obj.bh_multiple)
+        testcase_data.append(perf_obj.outperf)
+        testcase_data.append(perf_obj.cagr)
+        testcase_data.append(perf_obj.ann_mean)
+        testcase_data.append(perf_obj.ann_std)
+        testcase_data.append(perf_obj.sharpe)
+        testcase_data.append(perf_obj.sortino)
+        testcase_data.append(perf_obj.max_drawdown)
+        testcase_data.append(perf_obj.calmar)
+        testcase_data.append(perf_obj.max_dd_duration)
+        testcase_data.append(perf_obj.kelly_criterion)
+        testcase_data.append(perf_obj.start_d)
+        testcase_data.append(perf_obj.end_d)
+        testcase_data.append(perf_obj.num_samples)
+
+        for key, value in params.items():
+            if key not in self.df.columns:
+                #base_str_data.append(key)
+                self.df[key] = np.nan
+            testcase_data.append(value)
+
+            #self.df[key] = value
+            #print(key, value)
+        self.df.loc[len(self.df)] = testcase_data
+
+    def create_col(self):
+        lst = []
+
+        ticker = "XRP"
+        strategy = "EMA"
+        dataset = "training"
+        freq = "40min"
+        num_trades = 236
+        stat_mul = 0.95
+
+        lst.append(ticker)
+        lst.append(strategy)
+        lst.append(dataset)
+        lst.append(freq)
+        lst.append(num_trades)
+        lst.append(stat_mul)
+
+        return lst

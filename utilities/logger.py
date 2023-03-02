347d593d5182d80d3720d146cc4eaae227648e35
Usam Sersultanov <usam.sersultanov@efs-auto.de>
2023-03-02 18:47:10 +0100

    updated logging functionality of framework

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,12 +1,23 @@
 import logging
+import datetime
+import os.path
 
-file_name = "main_logger.log"
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+class Singleton(type):
+    _instances = {}
 
-handler = logging.FileHandler(file_name)
-handler.setFormatter(formatter)
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-logger.addHandler(handler)
+
+class Logger(metaclass=Singleton):
+    def __init__(self):
+        timestamp = datetime.datetime.now().strftime("logfile_%Y-%m-%d_%H-%M-%S.log")
+        log_filename = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "logs", timestamp))
+        logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', filename=log_filename, level=logging.DEBUG)
+        self.logger = logging.getLogger()
+
+    def get_logger(self):
+        return self.logger
